05d8f2d66ddc150fe0ec771987987f27c101e521
kevinbatongbakal <kevin.batongbakal@tooltwist.com>
2018-01-20 16:22:45 +0800

    Sample treeselect

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,13 @@
   <div class="container">
     <div>msg 1: {{ msg }}</div>
     <div>msg 2: {{ msg2 }}</div>
+    <div>
+      <treeselect
+        v-model="value"
+        :multiple="true"
+        :load-root-options="loadRootOptions"
+        />
+    </div>
   </div>
 </template>
 
@@ -14,13 +21,16 @@
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/share'
 import 'rxjs/add/observable/merge'
+import Treeselect from '@riophae/vue-treeselect'
 
 export default {
   name: "HelloWorld",
+  components: { Treeselect },
   data () {
     return {
       msg: '',
-      msg2: ''
+      msg2: '',
+      value: null
     }
   },
   
@@ -32,7 +42,10 @@ export default {
   subscriptions () {
     var msg$ = Observable.merge(this.setMsg$)
     var msg2$ = Observable.merge(this.setMsg2$)
-    return { msg: msg$, msg2: msg2$ }
+    return { 
+      msg: msg$, 
+      msg2: msg2$
+    }
   },
 
   created() {
@@ -43,7 +56,16 @@ export default {
     setTimeout( () => {
       this.setMsg2('The 1975')
     }, 2000)
+  },
+
+  methods: {
+    loadRootOptions(callback) {
+      var data = [{"id":"fruits","label":"Fruits","children":[{"id":"apple","label":"Apple 🍎"},{"id":"grapes","label":"Grapes 🍇"},{"id":"pear","label":"Pear 🍐"},{"id":"strawberry","label":"Strawberry 🍓"},{"id":"watermelon","label":"Watermelon 🍉"}]},{"id":"vegetables","label":"Vegetables","children":[{"id":"corn","label":"Corn 🌽"},{"id":"carrot","label":"Carrot 🥕"},{"id":"eggplant","label":"Eggplant 🍆"},{"id":"tomato","label":"Tomato 🍅"}]}];
+      setTimeout(() => callback(null, data), 2000)
+    }
   }
 
 };
 </script>
+
+<style src="@riophae/vue-treeselect/dist/vue-treeselect.min.css"></style>
