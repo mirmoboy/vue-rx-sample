b13858da0b6c95c3f0b8b43ddc68337336c26f7c
kevinbatongbakal <kevin.batongbakal@tooltwist.com>
2018-02-24 19:46:54 +0800

    updates

--- src/components/Croppie.vue
+++ src/components/Croppie.vue
@@ -6,14 +6,16 @@
         div
             img(ref='croppieContainer', :src='selectedImage')
         div
+            p {{ selectedImage }}
             button(@click='onCrop') Upload
         div
-            img(:src='image')
+            //- img(:src='image')
 </template>
 
 <script>
 import "croppie";
 import "croppie/croppie.css";
+import { setTimeout } from 'timers';
 
 export default {
   mounted() {
@@ -22,9 +24,9 @@ export default {
   data() {
     return {
       croppie: null,
-      selectedImage:
-        "https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg",
-      image: ''
+      selectedImage: "https://i0.wp.com/lovecuteanimals.objects.cdn.dream.io/wp-content/uploads/2016/01/Cute-Netherland-Dwarf-Rabbit.jpg",
+      image: '',
+      file: null
     };
   },
   methods: {
@@ -39,31 +41,43 @@ export default {
         boundary: { width: 400, height: 400 },
         showZoomer: true
       });
+      console.log('initCroppie')
+    },
+    destroyCroppie() {
+      this.croppie.destroy();
     },
     onCrop() {
-        const self = this
-        let options = {
-            format: 'blob',
-            quality: 1,
-            circle: false
-        }
+      const self = this;
+      let options = {
+        format: "blob",
+        quality: 1,
+        circle: false
+      };
       this.croppie.result(options).then(function(blob, qwe) {
-          console.log('onCrop')
-          self.image = blob
+        console.log("onCrop");
+        self.image = blob;
       });
     },
     onFileChange(e) {
+      const self = this;
+      self.destroyCroppie();
+      self.selectedImage = "https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg";
+      setTimeout( () => {
+          self.initCroppie();
+      }, 500)
+
       var files = e.target.files || e.dataTransfer.files;
-      if (!files.length)
-        return;
+      if (!files.length) return;
       this.createImage(files[0]);
     },
     createImage(file) {
       var image = new Image();
       var reader = new FileReader();
       var self = this;
+      console.log(file)
+      self.file = file
 
-      reader.onload = (e) => {
+      reader.onload = e => {
         self.image = e.target.result;
       };
       reader.readAsDataURL(file);
